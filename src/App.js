4915480bf782bc6115c5190a30e949d947f3e266
Austin <austin.hokun.wong@gmail.com>
2020-10-14 17:15:02 -0400

    added useeffect redone query

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/ui/Header';
 import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
 
   useEffect(() => { //whenenver useEffect gets run, add return () => {} which will run prior to useEffect running again
     const fetchItems = async () => {
-      const result = await Axios('https://www.breakingbadapi.com/api/characters')
-      console.log(result.data)
+      const result = await Axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
       setItems(result.data)
       setIsLoading(false);
     }
     fetchItems();
-  }, [])
+  }, [query])
 
 
   return (
   <div className='container'>
     <Header />
+    <Search getQuery={(q) => setQuery(q)}/>
     <CharacterGrid isLoading={isLoading} items={items}/>
   </div>
   )
---
+++ src/components/ui/Search.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const Search = ( { getQuery } ) => {
+    const [text, setText] = useState('');
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
+    return (
+        <section className='search'>
+            <form>
+                <input
+                onChange={(e) => onChange(e.target.value)}
+                type='text'
+                className='form-control'
+                placeholder='Search characters'
+                autoFocus
+                value={text}
+                ></input>  
+            </form>
+        </section>
+    )
+}
+
+export default Search
