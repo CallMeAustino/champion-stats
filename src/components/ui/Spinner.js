f3d7500563e261482f19a04726db0600a7b6bdd0
Austin <austin.hokun.wong@gmail.com>
2020-10-15 15:39:32 -0400

    added spinner specifics

--- src/components/ui/Spinner.js
+++ src/components/ui/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import SpinningGif from '../../img/spinner.gif'
 
 const Spinner = ({ items }) => {
-    if ( items.length >= 100 ) {
+    if ( items.length === 100 ) {
         return (
             <h1 className='center'>
                 Please type in a more specific search!
@@ -14,7 +14,8 @@ const Spinner = ({ items }) => {
             Enter a card name and wait to generate matching cards!
             {/* <img src={SpinningGif} style={{ width: '200px', margin: 'auto', display: 'block'}}/> */}
         </h1>
-    )}
+    )
+    }
 }
 
 export default Spinner
