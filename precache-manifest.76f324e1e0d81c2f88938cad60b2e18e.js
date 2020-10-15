4ff24983f8180394cce663866d944f8099f3ce7c
Austin <austin.hokun.wong@gmail.com>
2020-10-15 15:26:47 -0400

    Updates

--- precache-manifest.76f324e1e0d81c2f88938cad60b2e18e.js
+++ precache-manifest.76f324e1e0d81c2f88938cad60b2e18e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c828326bf630343d2a1ae6bf0ce03ac",
+    "revision": "349e49402d6fbd96c61c1b9fedc79aa1",
     "url": "/mtg-cardlookup/index.html"
   },
   {
-    "revision": "a51d98e8de52b1fa217b",
+    "revision": "20b381070b00995c334b",
     "url": "/mtg-cardlookup/static/css/main.f092734e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mtg-cardlookup/static/js/2.690b9eb5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a51d98e8de52b1fa217b",
-    "url": "/mtg-cardlookup/static/js/main.b1f61fbf.chunk.js"
+    "revision": "20b381070b00995c334b",
+    "url": "/mtg-cardlookup/static/js/main.159757d5.chunk.js"
   },
   {
     "revision": "3b7bc2ac4b3bf93f43de",
