42ec92d12eb0939e3917ee92f8893ba361b2e4a2
Austin <austin.hokun.wong@gmail.com>
2020-10-15 12:02:43 -0400

    added css

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -4,7 +4,7 @@ import Spinner from '../ui/Spinner';
 
 const CharacterGrid = ({ items, isLoading} ) => {
     return isLoading ? (<Spinner />) : (
-        <section>
+        <section className='cards'>
             {items.map(item => (
                 <CharacterItem key={item.char_id} item={item}></CharacterItem>
             ))}
