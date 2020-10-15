4911369bff38cb5748e2bb41afa5598df23216d3
Austin <austin.hokun.wong@gmail.com>
2020-10-15 14:55:46 -0400

    added setinterval for api requests

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,15 +1,34 @@
 import React, { useState } from 'react'
+import $ from 'jquery';
 
 const Search = ( { getQuery } ) => {
+
+    var typingTimer;                //timer identifier
+    var doneTypingInterval = 1000;  //time in ms (5 seconds)
+
+    //on keyup, start the countdown
+    $('#myInput').keyup(function(){
+        clearTimeout(typingTimer);
+        if ($('#myInput').val()) {
+            typingTimer = setTimeout(doneTyping, doneTypingInterval);
+        }
+    });
+
+    //user is "finished typing," do something
+    function doneTyping () {
+        getQuery($('#myInput').val())
+    }
     const [text, setText] = useState('');
+
     const onChange = (q) => {
         setText(q)
-        getQuery(q)
+        // getQuery(q)
     }
     return (
         <section className='search'>
             <form>
                 <input
+                id="myInput"
                 onChange={(e) => onChange(e.target.value)}
                 type='text'
                 className='form-control'
