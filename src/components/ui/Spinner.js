9b4e320ec9feaa7b7f3313610a47837807b05438
Austin <austin.hokun.wong@gmail.com>
2020-10-15 15:26:22 -0400

    fixed spinner msg

--- src/components/ui/Spinner.js
+++ src/components/ui/Spinner.js
@@ -4,7 +4,7 @@ import SpinningGif from '../../img/spinner.gif'
 const Spinner = () => {
     return (
         <h1 className='center'>
-            Enter a card name to generate cards!
+            Enter a card name and wait to generate matching cards!
             {/* <img src={SpinningGif} style={{ width: '200px', margin: 'auto', display: 'block'}}/> */}
         </h1>
     )
