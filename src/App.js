97df41988fa2f35f57b83d17b6c9f448a18b7a0b
Austin <austin.hokun.wong@gmail.com>
2022-08-09 15:48:15 -0400

    feature push test

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ const App = () => {
 
 
   useEffect(() => { //whenenver useEffect gets run, add return () => {} which will run prior to useEffect running again
-  setIsLoading(true); //test comment
-  setIsLoading(true); //unnecessary as this is set by default on line 13.
+  setIsLoading(true); //this is going to feature branch
 
     const fetchItems = async () => {
       const result = await mtg.card.where({name: query})
