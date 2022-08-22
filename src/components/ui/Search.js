82d312d499e1f2d33e8b3d81678cbf2c104a6700
Austin Wong <austinwong@lever-awong.mynetworksettings.com>
2022-08-22 13:53:08 -0400

    revert to autosearch

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -6,13 +6,13 @@ const Search = ( { getQuery } ) => {
     var typingTimer;                //timer identifier
     var doneTypingInterval = 2000;  //time in ms (2 seconds)
 
-    //on keyup, start the countdown
-    // $('#myInput').keyup(function(){
-    //     clearTimeout(typingTimer);
-    //     if ($('#myInput').val()) {
-    //         typingTimer = setTimeout(doneTyping, doneTypingInterval);
-    //     }
-    // });
+    // on keyup, start the countdown
+    $('#myInput').keyup(function(){
+        clearTimeout(typingTimer);
+        if ($('#myInput').val()) {
+            typingTimer = setTimeout(doneTyping, doneTypingInterval);
+        }
+    });
 
     //user is "finished typing," do something
     function doneTyping () {
@@ -25,15 +25,9 @@ const Search = ( { getQuery } ) => {
         // getQuery(q)
     }
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        doneTyping()
-        console.log('form submitted ✅');
-      };
-
     return (
         <section className='search'>
-            <form onSubmit={(handleSubmit)}>
+            <form>
                 <input
                 id="myInput"
                 onChange={(e) => onChange(e.target.value)}
@@ -43,7 +37,6 @@ const Search = ( { getQuery } ) => {
                 autoFocus
                 value={text}
                 ></input>  
-            <button type="submit">Submit</button>
             </form>
         </section>
     )
